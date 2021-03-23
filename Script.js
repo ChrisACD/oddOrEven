9a3509d12e21b6bf79a72ca73af8218149dae7c0
Djya <67033751+Chrisdjya@users.noreply.github.com>
2021-03-23 19:16:31 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+
+
+
+let numberToCheck = prompt("write a number");
+numberToCheck = Number(numberToCheck);
+let checkNumber = (numberToCheck % 2) ? "odd" : "even";
+console.log(numberToCheck);
+console.log(checkNumber);
+alert(`${numberToCheck}  is ${checkNumber} `);
